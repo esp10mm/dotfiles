3bb1c2a234f948c40d4b264b001ccc5b5038f8c9
Patrick Kuo <patrick5267@gmail.com>
2017-03-20 00:29:58 +0800

    add i3block config

--- link.sh
+++ link.sh
@@ -10,3 +10,4 @@ ln -sf ~/dotfiles/antigen.zsh ~/antigen.zsh
 ln -sf ~/dotfiles/my_smt.zsh-theme ~/.oh-my-zsh/custom/themes/my_smt.zsh-theme
 ln -sf ~/dotfiles/mytheme.sh ~/tmux-powerline/themes/mytheme.sh
 ln -sf ~/dotfiles/i3.config ~/.config/i3/config
+ln -sf ~/dotfiles/i3blocks.conf ~/.i3blocks.conf
