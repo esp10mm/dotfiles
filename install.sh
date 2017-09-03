b78d8c198fac33d10c0096182ba5187b37db5f64
Patrick Kuo <patrick5267@gmail.com>
2017-09-03 09:00:52 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+sudo apt-get install git curl i3blocks konsole tmux rofi tmuxinator ranger fonts-font-awesome
+
+cd ~
+git clone https://github.com/erikw/tmux-powerline.git
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# script for i3 gaps
+git clone https://github.com/maestrogerardo/i3-gaps-deb.git
